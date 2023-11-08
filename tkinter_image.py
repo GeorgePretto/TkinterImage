fb29914f2f223bc2e4a6494d83f478ce4ff6f447
George Pretto <geirgeantonipretto@gmail.com>
2023-11-08 20:54:53 -0300

    Primeira_imagem

---
+++ tkinter_image.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from PIL import ImageTk,Image
+
+
+root =Tk()
+img = Image.open("por.jpg")
+
+img_tk = ImageTk.PhotoImage(img)
+
+img_label = Label(root, image=img_tk)
+
+img_label.pack()
+
+root.mainloop()
