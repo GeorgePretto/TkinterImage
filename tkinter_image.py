eb7340db2056ad98bbf9b77a4d5a39cc410f96e3
George Pretto <geirgeantonipretto@gmail.com>
2023-11-17 20:56:59 -0300

    adicionar imagens

--- tkinter_image.py
+++ tkinter_image.py
@@ -1,13 +1,29 @@
 from tkinter import *
 from PIL import ImageTk,Image
+import os 
+
+
 
 
 root =Tk()
-img = Image.open("por.jpg")
+#Lista para os arquivos da pasta de imagens
+arquivos = os.listdir("imagens")
 
-img_tk = ImageTk.PhotoImage(img)
 
-img_label = Label(root, image=img_tk)
+#Variavel para armazenar as imagens
+imagens = []
+
+
+#Percorre a lista de arquivos
+for arquivo in arquivos:
+    #Abre a imagem
+    img = Image.open("imagens/"+arquivo)
+    #Adiciona imagem a lista
+    imagens.append(ImageTk.PhotoImage(img))
+
+
+#Exibe arquivos em um Label
+img_label = Label(root, image=imagens[1])
 
 img_label.pack()
 
---
+++ ze_do_codigo.py
@@ -0,0 +1,12 @@
+from tkinter import *
+root=Tk()
+
+root.title("Ze do codigo")
+
+ent= Entry(root, width=60)
+ent.pack()
+
+btn= Button(root, text="Click")
+btn.pack()
+
+root.mainloop()
